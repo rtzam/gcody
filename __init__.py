8ad836dd0c139f0dddfbb87864317ae488e69b6a
Ryan Zambrotta <rtzamis2@gmail.com>
2018-05-08 22:45:04 -0500

    Changed import syntax

--- __init__.py
+++ __init__.py
@@ -3,6 +3,9 @@
 
 # importing the core classes and functions
 
+#from .gcode_line import gcode_line
+from .gcode_settings import gcode_settings
 from .gcode import gcode
 from .readg import read
-from .gcode_settings import gcode_settings
+
+
