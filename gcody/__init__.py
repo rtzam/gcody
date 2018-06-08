f745278f38ef82e354604fad0130176b88c68f81
Ryan <ryanzambrotta2020@u.northwestern.edu>
2018-06-08 13:48:09 -0500

    Changed module names

---
+++ gcody/__init__.py
@@ -0,0 +1,9 @@
+# the init for the module gcody written by Ryan Zambrotta
+
+
+# importing the core classes and functions
+from .gsettings import gsettings
+from .gcode import gcode
+from .readg import read
+
+
