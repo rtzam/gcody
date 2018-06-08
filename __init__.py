773549eaf530a29be6bbba737635720bad1b8f69
Ryan <ryanzambrotta2020@u.northwestern.edu>
2018-06-08 14:22:22 -0500

    moved to folder

--- __init__.py
+++
@@ -1,9 +0,0 @@
-# the init for the module gcody written by Ryan Zambrotta
-
-
-# importing the core classes and functions
-from .gcode_settings import gcode_settings
-from .gcode import gcode
-from .readg import read
-
-
